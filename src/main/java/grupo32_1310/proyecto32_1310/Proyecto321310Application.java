778eff5958c6326d21d733f6d411b91b0be5e8d6
MILTON CESAR CARDONA <milton.cardona.mt@correo.usa.edu.co>
2021-10-14 16:59:16 -0500

    springboot

---
+++ src/main/java/grupo32_1310/proyecto32_1310/Proyecto321310Application.java
@@ -0,0 +1,13 @@
+package grupo32_1310.proyecto32_1310;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto321310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto321310Application.class, args);
+	}
+
+}
---
+++ src/test/java/grupo32_1310/proyecto32_1310/Proyecto321310ApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo32_1310.proyecto32_1310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto321310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
